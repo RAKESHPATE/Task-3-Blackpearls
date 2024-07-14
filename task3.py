5e0410aa55decd320e45c0f4b36f99da516d8a4c
RAKESHPATE <151204024+RAKESHPATE@users.noreply.github.com>
2024-07-14 19:20:50 +0530

    Create task3.py

---
+++ task3.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, redirect, url_for
+
+app = Flask(_name_)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/default")
+def default():
+    return render_template("layout.html")
+
+@app.route("/variable")
+def var():
+    user = "Geeksforgeeks"
+    return render_template("variable_example.html", name=user)
+
+@app.route("/if")
+def ifelse():
+    user = "Practice GeeksforGeeks"
+    return render_template("if_example.html", name=user)
+
+@app.route("/for")
+def for_loop():
+    list_of_courses = ['Java', 'Python', 'C++', 'MATLAB']
+    return render_template("for_example.html", courses=list_of_courses)
+
+@app.route("/choice/<pick>")
+def choice(pick):
+    if pick == 'variable':
+        return redirect(url_for('var'))
+    if pick == 'if':
+        return redirect(url_for('ifelse'))
+    if pick == 'for':
+        return redirect(url_for('for_loop'))
+
+if _name_ == "_main_":
+    app.run(debug=True)
+  
